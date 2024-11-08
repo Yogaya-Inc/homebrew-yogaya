8324939ceccd7b070b76b0eae0c2486a1833739a
omj-endo <t.endo@ordermade.jp>
2024-11-08 22:39:12 +0900

    update: yogaya cli

--- Formula/yogaya.rb
+++ Formula/yogaya.rb
@@ -1,33 +1,17 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Yogaya < Formula
-  desc ""
-  homepage ""
+  desc "Demo for Yogaya CLI"
+  homepage "https://yogaya.com/"
   url "https://github.com/Yogaya-Inc/yogaya-cli/releases/download/demo/yogaya"
   version "1.0.0"
   sha256 "3bbf0e2952007f0522f8193abb27b13909b4b95cf3ebe8e4432377c0c6450e92"
   license "NOASSERTION"
 
-  # depends_on "cmake" => :build
-
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "yogaya"
+    chmod 0755, bin/"yogaya"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test yogaya`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    assert_match "1.0.0", shell_output("#{bin}/yogaya --version")
   end
 end
