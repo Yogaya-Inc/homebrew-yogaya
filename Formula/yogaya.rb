ee9d291e965cf86ee407a82730dad41cbf5dd29f
omj-endo <t.endo@ordermade.jp>
2024-11-19 19:05:31 +0900

    update: yogaya cli

--- Formula/yogaya.rb
+++ Formula/yogaya.rb
@@ -1,10 +1,10 @@
 class Yogaya < Formula
-  desc "Demo for Yogaya CLI"
+  desc "Yogaya CLI"
   homepage "https://yogaya.com/"
-  url "https://github.com/Yogaya-Inc/yogaya-cli/releases/download/demo/yogaya"
-  version "1.0.0"
-  sha256 "3bbf0e2952007f0522f8193abb27b13909b4b95cf3ebe8e4432377c0c6450e92"
-  license "NOASSERTION"
+  url "https://github.com/Yogaya-Inc/yogaya-cli/releases/download/v1.1/yogaya"
+  version "1.0.1"
+  sha256 "530268f13c5d749639881299b3d1195787d70578ff7bc1b99fa258679326580b"
+  license "Apache-2.0"
 
   def install
     bin.install "yogaya"
@@ -12,6 +12,6 @@ class Yogaya < Formula
   end
 
   test do
-    assert_match "1.0.0", shell_output("#{bin}/yogaya --version")
+    assert_match "1.0.1", shell_output("#{bin}/yogaya --version")
   end
 end
