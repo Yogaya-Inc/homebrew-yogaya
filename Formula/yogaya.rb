c865cd62962e7f5db2a097a0f7f57cb3941af1f6
omj-endo <t.endo@ordermade.jp>
2024-12-03 04:49:31 +0900

    update: yogaya cli

--- Formula/yogaya.rb
+++ Formula/yogaya.rb
@@ -2,7 +2,7 @@ class Yogaya < Formula
   desc "Yogaya CLI"
   homepage "https://yogaya.com/"
   url "https://github.com/Yogaya-Inc/yogaya-cli/releases/download/v1.2/yogaya"
-  version "1.0.1"
+  version "1.2"
   sha256 "e6f59b6ac3ac3e541d961d015a6f0f835e8db422b6036c7335139ffc096fbc56"
   license "Apache-2.0"
 
