39cc138b714dc4d278dd6488e2a985f265626633
omj-endo <t.endo@ordermade.jp>
2024-12-03 04:46:54 +0900

    update: yogaya cli

--- Formula/yogaya.rb
+++ Formula/yogaya.rb
@@ -1,9 +1,9 @@
 class Yogaya < Formula
   desc "Yogaya CLI"
   homepage "https://yogaya.com/"
-  url "https://github.com/Yogaya-Inc/yogaya-cli/releases/download/v1.1/yogaya"
+  url "https://github.com/Yogaya-Inc/yogaya-cli/releases/download/v1.2/yogaya"
   version "1.0.1"
-  sha256 "530268f13c5d749639881299b3d1195787d70578ff7bc1b99fa258679326580b"
+  sha256 "e6f59b6ac3ac3e541d961d015a6f0f835e8db422b6036c7335139ffc096fbc56"
   license "Apache-2.0"
 
   def install
